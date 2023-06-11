ed5d1bdbe958ab4d4b5c8d607ca9955d473d2dc0
jlribeirojr <jlribeirojr@gmail.com>
2023-06-11 19:22:29 -0300

    Seller entities Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,22 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(500, "Telhados");
+
 		
-		System.out.println(obj);
+		Department obj = new Department(500, "Diretory");
+		
+		
+		Seller seller = new Seller (501, "jose ", "jose@gmail.com", new Date(), 20000.0,obj);
+		
+		System.out.println(seller);
 		
 	}
 
