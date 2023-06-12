99c4d53d9f8176b9abcd381d3ff2dd69162396df
jlribeirojr <jlribeirojr@gmail.com>
2023-06-11 21:24:19 -0300

    criação das classes DaoFactory e a impl. SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -15,6 +17,8 @@ public class Program {
 		
 		
 		Seller seller = new Seller (501, "jose ", "jose@gmail.com", new Date(), 20000.0,obj);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
 		
