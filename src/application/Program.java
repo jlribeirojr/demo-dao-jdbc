e3281bcf747b44e3b972b65dc49afab016eeb69c
jlribeirojr <jlribeirojr@gmail.com>
2023-06-13 20:52:15 -0300

    implementação do Update

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,17 +11,17 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+
 		System.out.println("=== TES 1: seller findById ====");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 
 		System.out.println("=== TES 2: seller findByDepartment ====");
-		Department department = new Department(2,null);
+		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
-		
+
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
@@ -32,9 +32,16 @@ public class Program {
 			System.out.println(obj);
 		}
 		System.out.println("=== TES 4: seller Insert ====");
-		Seller newSeller = new Seller(null,"Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
-	}
+		
+		System.out.println("=== TES 5: seller Update ====");
+		seller = sellerDao.findById(1);
+		seller.setName("Matha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
+		}
+		
 
 }
