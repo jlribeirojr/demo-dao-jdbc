c416aa534139b07ea5036fab25ac5accf7e6ae01
jlribeirojr <jlribeirojr@gmail.com>
2023-06-12 22:23:25 -0300

    reutilizando a instanciação

--- src/application/Program.java
+++ src/application/Program.java
@@ -10,7 +10,7 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
-		Seller seller = sellerDao.findById(2);
+		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
 		
 	}
