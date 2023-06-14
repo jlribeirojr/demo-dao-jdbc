7b773ead9947a06409e282e093119dc709cd941d
jlribeirojr <jlribeirojr@gmail.com>
2023-06-13 21:05:10 -0300

    Implementação do DELETE

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -11,7 +12,7 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
-
+		Scanner sc = new Scanner(System.in);
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 
 		System.out.println("=== TES 1: seller findById ====");
@@ -41,6 +42,13 @@ public class Program {
 		seller.setName("Matha Waine");
 		sellerDao.update(seller);
 		System.out.println("Update completed");
+		
+		System.out.println("=== TES 6: seller Delete ====");
+		System.out.println("Enter with Id for delete?");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed");
+		sc.close();
 		}
 		
 
